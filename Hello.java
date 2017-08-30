906d705ac52538c6f95385601c7e44523e61ccdc
Krerk Piromsopa <Krerk.p@chula.ac.th>
2017-08-30 17:28:19 +0700

    more changes

--- Hello.java
+++ Hello.java
@@ -9,6 +9,7 @@ class Hello {
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
+        System.out.println("  Demo  - Krerk Piromsopa, Ph.D.");
     }
     
     public static void main(String [] argv) {
